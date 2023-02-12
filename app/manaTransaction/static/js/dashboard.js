71d7eddf54797c82cd15d8dc7c62d15d95d81cc3
DungTran582001 <tranmanhdung582001@gmail.com>
2023-02-12 20:17:28 +0700

    edit num2cur function to display balance in dashboard page

--- app/manaTransaction/static/js/dashboard.js
+++ app/manaTransaction/static/js/dashboard.js
@@ -1,27 +1,31 @@
+
+
+
+
 var $table = document.getElementById("myTable"),
-// number of rows per page
-$n = 5,
-// number of rows of the table
-$rowCount = $table.rows.length,
-// get the first cell's tag name (in the first row)
-$firstRow = $table.rows[0].firstElementChild.tagName,
-// boolean var to check if table has a head row
-$hasHead = ($firstRow === "TH"),
-// an array to hold each row
-$tr = [],
-// loop counters, to start count from rows[1] (2nd row) if the first row has a head tag
-$i,$ii,$j = ($hasHead)?1:0,
-// holds the first row if it has a (<TH>) & nothing if (<TD>)
-$th = ($hasHead?$table.rows[(0)].outerHTML:"");
+	// number of rows per page
+	$n = 5,
+	// number of rows of the table
+	$rowCount = $table.rows.length,
+	// get the first cell's tag name (in the first row)
+	$firstRow = $table.rows[0].firstElementChild.tagName,
+	// boolean var to check if table has a head row
+	$hasHead = ($firstRow === "TH"),
+	// an array to hold each row
+	$tr = [],
+	// loop counters, to start count from rows[1] (2nd row) if the first row has a head tag
+	$i, $ii, $j = ($hasHead) ? 1 : 0,
+	// holds the first row if it has a (<TH>) & nothing if (<TD>)
+	$th = ($hasHead ? $table.rows[(0)].outerHTML : "");
 // count the number of pages
 var $pageCount = Math.ceil($rowCount / $n);
 // if we had one page only, then we have nothing to do ..
 if ($pageCount > 1) {
 	// assign each row outHTML (tag name & innerHTML) to the array
-	for ($i = $j,$ii = 0; $i < $rowCount; $i++, $ii++)
+	for ($i = $j, $ii = 0; $i < $rowCount; $i++, $ii++)
 		$tr[$ii] = $table.rows[$i].outerHTML;
 	// create a div block to hold the buttons
-	$table.insertAdjacentHTML("afterend","<div id='buttons'></div");
+	$table.insertAdjacentHTML("afterend", "<div id='buttons'></div");
 	// the first sort, default page is the first one
 	sort(1);
 }
@@ -32,32 +36,32 @@ function sort($p) {
 	** to be displayed on the selected page,
 	** ($s) the start point .. the first row in each page, Do The Math
 	*/
-	var $rows = $th,$s = (($n * $p)-$n);
-	for ($i = $s; $i < ($s+$n) && $i < $tr.length; $i++)
+	var $rows = $th, $s = (($n * $p) - $n);
+	for ($i = $s; $i < ($s + $n) && $i < $tr.length; $i++)
 		$rows += $tr[$i];
-	
+
 	// now the table has a processed group of rows ..
 	$table.innerHTML = $rows;
 	// create the pagination buttons
-	document.getElementById("buttons").innerHTML = pageButtons($pageCount,$p);
+	document.getElementById("buttons").innerHTML = pageButtons($pageCount, $p);
 	// CSS Stuff
-	document.getElementById("id"+$p).setAttribute("class","active");
+	document.getElementById("id" + $p).setAttribute("class", "active");
 }
 
 
 // ($pCount) : number of pages,($cur) : current page, the selected one ..
-function pageButtons($pCount,$cur) {
+function pageButtons($pCount, $cur) {
 	/* this variables will disable the "Prev" button on 1st page
 	   and "next" button on the last one */
-	var	$prevDis = ($cur == 1)?"disabled":"",
-		$nextDis = ($cur == $pCount)?"disabled":"",
+	var $prevDis = ($cur == 1) ? "disabled" : "",
+		$nextDis = ($cur == $pCount) ? "disabled" : "",
 		/* this ($buttons) will hold every single button needed
 		** it will creates each button and sets the onclick attribute
 		** to the "sort" function with a special ($p) number..
 		*/
-		$buttons = "<input type='button' value='<< Prev' onclick='sort("+($cur - 1)+")' "+$prevDis+">";
-	for ($i=1; $i<=$pCount;$i++)
-		$buttons += "<input type='button' id='id"+$i+"'value='"+$i+"' onclick='sort("+$i+")'>";
-	$buttons += "<input type='button' value='Next >>' onclick='sort("+($cur + 1)+")' "+$nextDis+">";
+		$buttons = "<input type='button' value='<< Prev' onclick='sort(" + ($cur - 1) + ")' " + $prevDis + ">";
+	for ($i = 1; $i <= $pCount; $i++)
+		$buttons += "<input type='button' id='id" + $i + "'value='" + $i + "' onclick='sort(" + $i + ")'>";
+	$buttons += "<input type='button' value='Next >>' onclick='sort(" + ($cur + 1) + ")' " + $nextDis + ">";
 	return $buttons;
 }
