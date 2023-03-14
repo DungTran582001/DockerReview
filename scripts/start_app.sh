6de61ae49fc9ee3ff98a96f146b3ba08730e5217
DungTran582001 <tranmanhdung582001@gmail.com>
2023-03-14 20:38:03 +0700

    edit public IPv4 of server

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["52.87.255.195"]/' /home/ubuntu/djangoproject/app/settings.py
+sed -i 's/\[]/\["52.201.173.57"]/' /home/ubuntu/djangoproject/app/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
