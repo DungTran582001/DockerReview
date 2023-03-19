a20e2e6443ae2818a110b0fc3ffc0e5cdc55cece
DungTran582001 <tranmanhdung582001@gmail.com>
2023-03-19 14:30:41 +0700

    edit config deployment files

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,8 +4,8 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/djangoproject/nginx/nginx.conf /etc/nginx/sites-available/djangoproject
-sudo ln -s /etc/nginx/sites-available/djangoproject /etc/nginx/sites-enabled/
+sudo cp /home/ubuntu/reviewdocker/nginx/nginx.conf /etc/nginx/sites-available/reviewdocker
+sudo ln -s /etc/nginx/sites-available/reviewdocker /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/djangoproject /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
